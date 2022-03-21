ddd4f009e4d4175567241e3fbc73602cc27156f2
Bryon <pearson.bryon90@gmail.com>
2022-03-21 11:58:37 -0700

    updating ElevenNote

--- ElevenNote.Data/ApplicationDbContext.cs
+++ ElevenNote.Data/ApplicationDbContext.cs
@@ -2,14 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ElevenNote.Data.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace ElevenNote.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options){
-            
-        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options){}
+                    public DbSet<UserEntity> Users {get;set;}
     }
 }
