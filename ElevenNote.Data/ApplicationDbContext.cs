be8c441e3212cae35a20cbbd62f4bd32e2b7ae89
Bryon <pearson.bryon90@gmail.com>
2022-03-22 14:45:53 -0700

    upadated both UserServices, plus more

--- ElevenNote.Data/ApplicationDbContext.cs
+++ ElevenNote.Data/ApplicationDbContext.cs
@@ -10,6 +10,6 @@ namespace ElevenNote.Data
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options){}
-                    public DbSet<UserEntity> Users {get;set;}
+                    public DbSet<UserEntity> User{get;set;}
     }
 }
