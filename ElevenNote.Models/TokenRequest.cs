6f372152e7017aeba4cfe3e19fe9e76deca755d9
Bryon <pearson.bryon90@gmail.com>
2022-03-24 16:43:38 -0700

    adding Tokens

---
+++ ElevenNote.Models/TokenRequest.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Models.Token
+{
+    public class TokenRequest
+    {
+        [Required]
+        public string Username{get;set;}
+
+        [Required]
+        public string Password{get;set;}
+    }
+}
